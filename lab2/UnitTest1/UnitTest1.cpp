5d191615c305228ec60f01b7700d655715ba1f4c
VetaSolo <74370323+VetaSolo@users.noreply.github.com>
2021-11-25 00:02:05 +0300

    Add files via upload

---
+++ lab2/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,95 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../lab2/Sort_Search_func.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(lab2)
+	{
+	public:
+		
+		TEST_METHOD(QuickSort_test)
+		{
+			int* arr = nullptr;
+			arr = new int[4];
+			for (int i = 0; i < 4; i++)
+			{
+				arr[i] = 4 - i;
+			}
+			QuickSort(arr, 0, 3);
+			int* arr_true = nullptr;
+			arr_true = new int[4];
+			arr_true[0] = 1;
+			arr_true[1] = 2;
+			arr_true[2] = 3;
+			arr_true[3] = 4;
+			Assert::IsTrue(arr[0] == arr_true[0]);
+			Assert::IsTrue(arr[3] == arr_true[3]);
+			Assert::IsTrue(arr[2] == arr_true[2]);
+			Assert::IsTrue(arr[1] == arr_true[1]);
+			delete[] arr;
+			delete[] arr_true;
+		}
+		TEST_METHOD(BinarySearch_test)
+		{
+			int* arr = nullptr;
+			arr = new int[4];
+			for (int i = 0; i < 4; i++)
+			{
+				arr[i] = 4 - i;
+			}
+			int number=0;
+			number = BinarySearch(arr, 0, 4, 1);
+			Assert::IsFalse(number == 0);
+			Assert::IsFalse(number == 1);
+			Assert::IsFalse(number == 2);
+			delete[] arr;
+		}
+		TEST_METHOD(InsertSort_test)
+		{
+			int* arr = nullptr;
+			arr = new int[4];
+			for (int i = 0; i < 4; i++)
+			{
+				arr[i] = 4 - i;
+			}
+			InsertSort(arr,4);
+			int* arr_true = nullptr;
+			arr_true = new int[4];
+			arr_true[0] = 1;
+			arr_true[1] = 2;
+			arr_true[2] = 3;
+			arr_true[3] = 4;
+			Assert::IsTrue(arr[0] == arr_true[0]);
+			Assert::IsTrue(arr[3] == arr_true[3]);
+			Assert::IsTrue(arr[2] == arr_true[2]);
+			Assert::IsTrue(arr[1] == arr_true[1]);
+			delete[] arr;
+			delete[] arr_true;
+		}
+		TEST_METHOD(CountingSort_test)
+		{
+			char* string = nullptr;
+
+			string = new char[5];
+			string[0] = 'b';
+			string[1] = 'd';
+			string[2] = 'c';
+			string[3] = 'a';
+			string[4] = '\0';
+			CountingSort(string, 4);
+			Assert::AreEqual("abcd", string);
+			delete[] string;
+		}
+		TEST_METHOD(BogoSort_test)
+		{
+			int* arr = nullptr;
+			arr = Get_array(32, 10);
+			BogoSort(arr, 10);
+			Assert::IsTrue(correct(arr, 10));
+			delete[] arr;
+		}
+	};
+
+}
---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,209 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "Sort_Search_func.h"
+using namespace std;
+/*
+int* Get_array(int max, int size)
+{
+    if (max > 0 && size > 0)
+    {
+        int* arr = new int[size];
+        int i;
+        srand(time(0));
+
+        //generate an entire random array of SIZE items 1 to MAX
+        arr[0] = max/3 + rand() % max;
+        for (i = 1; i < size; i++)
+            arr[i] = 1 + rand() % max;
+        return arr;
+    }
+}
+void Print_array(int *array, int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << array[i] << " "; // вывод результата на экране
+    cout << endl;
+}
+int Get_max()
+{
+    srand(time(0));
+    int max= rand();
+    return max;
+}
+
+int BinarySearch(int* arr, int left, int right, int search_item)
+{
+    if (arr != nullptr && left > 0 && right > 0)
+    {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == search_item) {
+                return mid;
+            }
+            else if (arr[mid] < search_item) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; //if search item doesn't exsist
+    }
+    else
+    {
+        cout << "Wrong input" << endl;
+    }
+}
+void QuickSort(int* array, int first, int last) 
+{
+    if (array != nullptr)
+    {
+        int pivot, left, temp, right;
+
+        if (first < last) {
+            pivot = first;
+            right = first;
+            left = last;
+
+            while (right < left)
+            {
+                while (array[right] <= array[pivot] && right < last)
+                    right++;
+                while (array[left] > array[pivot])
+                    left--;
+                if (right < left)
+                {
+                    temp = array[right];
+                    array[right] = array[left];
+                    array[left] = temp;
+                }
+            }
+
+            temp = array[pivot];
+            array[pivot] = array[left];
+            array[left] = temp;
+            QuickSort(array, first, left - 1);
+            QuickSort(array, left + 1, last);
+        }
+
+    }
+    else
+    {
+        cout << "Empty array" << endl;
+    };
+}
+void InsertSort(int* array, int size)
+{
+    if (array!=nullptr && size > 0)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int i = 1; i < size; i++) {
+                for (int j = i; j > 0 && array[j - 1] > array[j]; j--) {//until j>0 and item  j-1 > j. swap j and j-1
+                    int tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+    else {
+        cout << "Wrong input" << endl;
+    }
+}
+int correct(int* arr, int size) 
+{
+    while (size-- > 0)
+        if (arr[size - 1] > arr[size])
+            return 0;
+    return 1;
+}
+void BogoSort(int* arr, int size) {
+    while (correct(arr, size)!= 1)
+        for (int i = 0; i < size; ++i) // swap arr[i] to rand items until sort it
+        {
+            int save = arr[i];
+            int rand_i = rand() % size;
+            arr[i] = arr[rand_i];
+            arr[rand_i] = save;
+        }
+}
+int MaxChar(char* string, int size)
+{
+    if (string && size > 0)
+    {
+        int max = string[0];
+        //finds the maximum number in a character array
+        for (int i = 1; i < size; i++)
+        {
+            if (max< string[i])
+                max = (int)string[i];
+        }
+        return max;
+    }
+}
+void CountingSort(char* string, int size) {
+    if (string && size > 0)
+    {
+        int max;
+        max = MaxChar(string, size);
+        char* new_arr = new char[max + 1];
+        // fills the array with 0
+        memset(new_arr, 0, sizeof(*string) * (max + 1));
+        for (int i = 0; i < size; ++i)
+        {
+            ++new_arr[string[i]];
+        }
+        int index = 0;
+        //if j < new_arr then swap string[index] with i and increase index
+        for (int i = 0; i < max + 1; ++i) {
+            for (int j = 0; j < new_arr[i]; ++j)
+            {
+                string[index++] = i;
+            }
+        }
+        delete[] new_arr;
+        new_arr = nullptr;
+    }
+}
+*/
+
+int main()
+{
+
+    int* array_1 = nullptr, * array_2 = nullptr;
+    int max = Get_max();
+    cout << "Max : " << max << endl;
+    clock_t begin, end;
+    double sec;
+    srand(time(0));
+    
+    for (int i = 10; i < 100001; i *= 10)
+    {
+        begin = clock();
+        array_2 = Get_array(max, i);
+        QuickSort(array_2, 0, i - 1);
+        end = clock();
+        sec = ((double)end - (double)begin) / CLOCKS_PER_SEC;
+        cout << "Time of QuickSort " << i << " elements " << sec << "sec" << endl;
+        delete[] array_2;
+        array_2 = nullptr;
+    }
+    
+    for (int i = 10; i < 100001; i *= 10)
+    {
+        begin = clock();
+        array_1 = Get_array(max, i);
+        InsertSort(array_1, i);
+        end = clock();
+        sec = ((double)end - (double)begin) / CLOCKS_PER_SEC;
+        cout << "Time of InsertionSort " << i << " elements " << sec << "sec" << endl;
+        delete[] array_1;
+        array_1 = nullptr;
+    }
+    return 0;
+}
